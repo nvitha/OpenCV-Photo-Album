298b5c50c31917fb1653ab98ef152766f8341cbf
chauncyc <ccullitan@zagmail.gonzaga.edu>
2015-12-05 19:00:50 -0800

    Rudimentary user interface

--- app/src/main/java/edu/gonzaga/opencvtest/MainActivity.java
+++ app/src/main/java/edu/gonzaga/opencvtest/MainActivity.java
@@ -2,15 +2,26 @@ package edu.gonzaga.opencvtest;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.database.sqlite.*;
+
+import java.io.File;
+import java.util.ArrayList;
 
 import edu.gonzaga.opencvtest.R;
 
 public class MainActivity extends Activity {
     private static final String TAG = "Main";
+    private ArrayList<File> pictures; //should be sorted
+    private int currentPictureIndex;
+    //TODO: Pass images to pictures from SQL query
 
     public MainActivity() {
         Log.i(TAG, "Start main");
@@ -31,6 +42,57 @@ public class MainActivity extends Activity {
                 startActivity(intent);
             }
         });
+
+
+        // populate images
+        populatePicturesDefault();
+        //TODO: Populate pictures with results of SQL query
+        //set listeners
+        Button nextButton = (Button) findViewById(R.id.next);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("nextButton was clicked");
+                nextPhoto();
+            }
+        });
+        Button prevButton = (Button) findViewById(R.id.prev);
+        prevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prevPhoto();
+            }
+        });
+
+        setImageToCurrent();
+    }
+
+    public void populatePicturesDefault() {
+        pictures = new ArrayList<>();
+        currentPictureIndex = 0;
+        String dirName = Environment.getExternalStorageDirectory().getPath();
+        File dir = new File(dirName);
+
+        for (File f : dir.listFiles()) {
+            if (f.toString().endsWith(".jpg")) pictures.add(f);
+        }
+    }
+
+    public void setImageToCurrent() {
+        ImageView img = (ImageView) findViewById(R.id.imageView);
+        Bitmap bmp = BitmapFactory.decodeFile(pictures.get(currentPictureIndex).toString());
+        img.setImageBitmap(bmp);
+    }
+
+    public void nextPhoto() {
+        if (currentPictureIndex < pictures.size() - 1) currentPictureIndex++;
+        System.out.println("Index: " + currentPictureIndex);
+        setImageToCurrent();
+    }
+
+    public void prevPhoto() {
+        currentPictureIndex = Math.max(0, currentPictureIndex - 1);
+        setImageToCurrent();
     }
 
     @Override
