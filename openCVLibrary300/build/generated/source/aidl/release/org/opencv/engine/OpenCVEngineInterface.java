e1a6938bee497a07462f2b72d58b88bd816eeee5
nvitha <nvitha@zagmail.gonzaga.edu>
2015-12-10 05:01:12 -0800

    Tried to fix storage -- I think our reading off the database is borked

--- openCVLibrary300/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary300/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /home/user/AndroidStudioProjects/OpenCV-Photo-Album/openCVLibrary300/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\Nicholas\\Documents\\Github\\OpenCV-Photo-Album\\openCVLibrary300\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
